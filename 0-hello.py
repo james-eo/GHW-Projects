d0173e79bdd867ca9d76c9c5da5028afd3bfdeda
james-eo <jameseokonkwo@gmail.com>
2024-01-06 17:40:08 +0100

    Hello World!

---
+++ 0-hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def hello_world():
+    """Defines a function that prints Hello, World!"""
+
+    message = "Hello, World!"
+    return message
+
+# Calling the function to get the message
+greetings = hello_world()
+print(greetings)
